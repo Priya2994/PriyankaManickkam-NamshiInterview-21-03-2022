f38220eea4c3753047984f41bd099d995976c3d7
priyankaalta2 <87259275+priyankaalta2@users.noreply.github.com>
2022-03-21 19:17:31 +0400

    task 1

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React, { useState } from 'react';
+
+const Grid = [...Array(16).keys()];
+
+
+function App() {
+const [matrix,setMatrix] = useState([]);
+const handleClick = (id) => {
+    if(matrix.length >= 2) {
+        const findCurIndex = matrix.indexOf(id);
+        if(findCurIndex > -1 && matrix.length <= 2) {
+           return 
+        } else {
+            const splicedVal = [...matrix].splice(findCurIndex,1);
+             setMatrix([...splicedVal, id]);
+        }
+    } else if(matrix.length < 2){
+    setMatrix([...matrix, id]);
+}
+    
+  };
+  
+    return ( 
+      <div className = "App">
+        <div className = 'matrix'> 
+        {Grid.map((item, key) => (
+              <div key={item} style={{
+                backgroundColor: matrix.indexOf(item) > -1 ? 'red' : 'blue'
+              }} onClick={() => handleClick(item)}/>
+            ))}
+        </div>   
+        </div>
+    );
+}
+
+export default App;
